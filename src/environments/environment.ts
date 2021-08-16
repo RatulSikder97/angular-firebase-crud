f2eaaf8812bacf78889203c1214d075c25f865b9
ratul.sikder <ratul.sikder@rokomari.com>
2021-08-17 00:05:51 +0600

    Project Init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBU1aZyQFbr9CZzIyxtBs2Dck2wI8Oebvk',
+    authDomain: 'angular-crud-c5213.firebaseapp.com',
+    projectId: 'angular-crud-c5213',
+    storageBucket: 'angular-crud-c5213.appspot.com',
+    messagingSenderId: '816097539418',
+    appId: '1:816097539418:web:7f773cbe6f1b75184aaf63',
+  },
 };
 
 /*
